29f845ed6c9095026c015cc6f947d14f207dde27
Matenk <167009609+Matenk@users.noreply.github.com>
2024-07-05 19:32:18 +0300

    Add files via upload

---
+++ iter_v2.py
@@ -0,0 +1,26 @@
+class EvenNumbers:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+
+    def __iter__(self):
+        return self
+
+
+    def __next__(self):
+        if self.start < self.end:
+            x = self.start
+            self.start += 1
+            return x
+        else:
+            raise StopIteration
+
+
+
+
+
+en = EvenNumbers(10, 25)
+for i in en:
+    if i % 2 == 0:
+        print(i)
